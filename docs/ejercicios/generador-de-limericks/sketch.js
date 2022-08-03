e4bc5ad27828473c99e55cbf6119da069c980cdd
Jaime Rodriguez Gomez <jaimerodriguezgo@gmail.com>
2022-08-03 17:10:18 -0500

    udpate

--- docs/ejercicios/generador-de-limericks/sketch.js
+++ docs/ejercicios/generador-de-limericks/sketch.js
@@ -20,62 +20,34 @@ let v3r = 0;
 let v4r = 0;
 let v5r = 0;
 
-// v1 - protagonista A
-// v2 - acción A
-// v3 - contexto pt1 B
-// v4 - contexto pt2 B
-// v5 - epíteto A
+let tabla;
+let cantidadOpciones;
+
+// RIMA - VERSO - FUNCIÓN NARRATIVA
+// A    - v1    - personaje
+// A    - v2    - acción del personaje
+// B    - v3    - acción complementaria del personaje / evento del contexto 1 B
+// B    - v4    - acción complementaria del personaje / evento del contexto 2 B
+// A    - v5    - final o conclusión
 
 function setup() {
-  cv = createCanvas(windowWidth, windowHeight);
+  cv = createCanvas(innerWidth, innerHeight);
 
   font = loadFont('assets/WorkSans-Regular.ttf');
 
-  v1[0] = 'Un acróbata de apariencia cansada';
-  v1[1] = 'Un congresista de energía apagada';
-  v1[2] = 'Una vendedora con su ropa completamente mojada';
-  v1[3] = 'Una ciclista vestida como estrella iluminada';
-  v1[4] = 'Un indigente de melena enmarañada';
-  v1[5] = 'Una vigilante que termina su jornada';
-  v1[6] = 'Doña María busca los ingredientes para hacer su ensalada';
+  cantidadOpciones = tabla.getRowCount();
 
-  v2[0] = 'da tumbos entre la multitúd pesada';
-  v2[1] = 'camina por la escalera empinada';
-  v2[2] = 'se entristece por no haber vendido nada';
-  v2[3] = 'esquiva como puede a quienes salen de la nada';
-  v2[4] = 'pide dinero para comerse una empanada';
-  v2[5] = 'camina hacia la estación La Alborada';
-  v2[6] = 've con dificultad a través de sus gafas empañadas';
+  v1r = int(random(0, cantidadOpciones));
+  v2r = int(random(0, cantidadOpciones));
+  v3r = int(random(0, cantidadOpciones));
+  v4r = int(random(0, cantidadOpciones));
+  v5r = int(random(0, cantidadOpciones));
 
-  v3[0] = 'un señor grita';
-  v3[1] = 'una mujer tirita';
-  v3[2] = 'una perra se agita';
-  v3[3] = 'su conciencia levita';
-  v3[4] = 'de repente se excita';
-  v3[5] = 'se desmaya un curita';
-  v3[6] = 'de repente se excita';
-
-  v4[0] = 'alguien lo imita';
-  v4[1] = 'se tropieza con una pita';
-  v4[2] = 'gaña como un chita';
-  v4[3] = 'pero nadie milita';
-  v4[4] = 'y alguien se lo facilita';
-  v4[5] = 'recuerda a su abuelita';
-  v4[6] = 'y alguien se lo facilita';
-
-  v5[0] = 'la gente se extrañó al ver al vendedor de cocadas.';
-  v5[1] = 'nunca se volvió a ver una situación tan disparatada.';
-  v5[2] = 'justo cuando la plaza está más abarrotada.';
-  v5[3] = 'un gran esfuerzo para conseguir su ensalada.';
-  v5[4] = 'nadie notó que tenía una coartada.';
-  v5[5] = 'siempre buscó donde pudo su mermelada.';
-  v5[6] = 'mal momento para ir como sardinas enlatadas.';
-
-  v1r = int(random(0, 7));
-  v2r = int(random(0, 7));
-  v3r = int(random(0, 7));
-  v4r = int(random(0, 7));
-  v5r = int(random(0, 7));
+  v1 = tabla.getColumn('v1');
+  v2 = tabla.getColumn('v2');
+  v3 = tabla.getColumn('v3');
+  v4 = tabla.getColumn('v4');
+  v5 = tabla.getColumn('v5');
 }
 
 function draw() {
@@ -91,22 +63,18 @@ function draw() {
   textSize(30);
   textFont(font);
   textAlign(LEFT);
+
   text(v1[v1r], 140, yv1);
   text(v2[v2r], 140, yv2);
   text(v3[v3r], 140, yv3);
   text(v4[v4r], 140, yv4);
   text(v5[v5r], 140, yv5);
 
-  /*
-  fill(110);
-  textAlign(RIGHT);
-  text('rimas', 120, yv1 - 50);
-  text('[ada]', 120, yv1);
-  text('[ada]', 120, yv2);
-  text('[ita]', 120, yv3);
-  text('[ita]', 120, yv4);
-  text('[ada]', 120, yv5);
-  */
+  // console.log(tabla.getString(0, 0)); // Un acróbata de apariencia cansada
+  // console.log('filas ' + tabla.getRowCount()); // 7
+  // console.log('columnas ' + tabla.getColumnCount()); // 5
+  // console.log('column name ' + tabla.getColumn('v1'));
+  console.log(v1[0]); // Un acróbata de apariencia cansada
 
   fill(0, 255, 255);
   textAlign(LEFT);
@@ -117,20 +85,21 @@ function draw() {
 
 function preload() {
   loadFont('assets/WorkSans-Regular.ttf');
+  tabla = loadTable('assets/versos.csv', 'csv', 'header');
 }
 
 function keyPressed() {
-  v1r = int(random(0, 7));
-  v2r = int(random(0, 7));
-  v3r = int(random(0, 7));
-  v4r = int(random(0, 7));
-  v5r = int(random(0, 7));
+  v1r = int(random(0, cantidadOpciones));
+  v2r = int(random(0, cantidadOpciones));
+  v3r = int(random(0, cantidadOpciones));
+  v4r = int(random(0, cantidadOpciones));
+  v5r = int(random(0, cantidadOpciones));
 }
 
 function touchStarted() {
-  v1r = int(random(0, 7));
-  v2r = int(random(0, 7));
-  v3r = int(random(0, 7));
-  v4r = int(random(0, 7));
-  v5r = int(random(0, 7));
+  v1r = int(random(0, cantidadOpciones));
+  v2r = int(random(0, cantidadOpciones));
+  v3r = int(random(0, cantidadOpciones));
+  v4r = int(random(0, cantidadOpciones));
+  v5r = int(random(0, cantidadOpciones));
 }
